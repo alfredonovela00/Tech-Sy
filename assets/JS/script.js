d4090326ca397a251b003ed8b84a95b2165ed2ed
Alfredo Novela <alfredonovelaa@gmail.com>
2023-11-02 00:36:44 +0200

    update
    width

---
+++ assets/JS/script.js
@@ -0,0 +1,16 @@
+// Função para rolagem suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 70, // Ajuste para acomodar a barra de navegação
+                behavior: 'smooth'
+            });
+        }
+    });
+})
